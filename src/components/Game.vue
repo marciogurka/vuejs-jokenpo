137dd8fae52d738fc9773460c92a06f92c1e09f1
Márcio José Gurka Jr <marciogurkajr@gmail.com>
2018-08-07 21:06:26 -0300

    feat: Componente Game adicionado

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,11 +1,28 @@
 <template>
   <div class="game">
+    <div class="container">
+      <h1 class="title">
+        Jokenpo
+      </h1>
+      <p class="subtitle">
+        Start the game below!
+      </p>
+      <div class="columns is-mobile">
+        <Player></Player>
+        <Board></Board>
+        <Player></Player>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Player from '@/components/Player';
+import Board from '@/components/Board';
+
 export default {
   name: 'Game',
+  components: { Player, Board },
   data() {
     return {
       msg: 'Welcome to Your Vue.js App',
@@ -16,18 +33,4 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
