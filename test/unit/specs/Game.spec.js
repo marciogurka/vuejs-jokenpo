c3aa112ea1c4edd2aa307928f33aeabf0fe4c154
Márcio José Gurka Jr <marciogurkajr@gmail.com>
2018-08-09 13:07:42 -0300

    feat: atualizado o teste do componente Game

--- test/unit/specs/Game.spec.js
+++ test/unit/specs/Game.spec.js
@@ -1,14 +1,34 @@
-import { shallowMount } from '@vue/test-utils';
+import { mount } from '@vue/test-utils';
 import Game from '@/components/Game';
+import Player from '@/components/Player';
+import Board from '@/components/Board';
 
 describe('GameSpec.vue', () => {
   let cmp;
 
+  /* Mouting the component before each test */
   beforeEach(() => {
-    cmp = shallowMount(Game, {});
+    cmp = mount(Game, {});
   });
 
   it('has the expected html structure', () => {
     expect(cmp.element).toMatchSnapshot();
+  });
+
+  it('is a Vue Component', () => {
+    expect(cmp.isVueInstance()).toBe(true);
+  });
+
+  it('is a Game component', () => {
+    expect(cmp.is(Game)).toBe(true);
+  });
+
+  it('has 2 Player component', () => {
+    const numberPlayers = cmp.findAll(Player).length;
+    expect(numberPlayers).toBe(2);
+  });
+
+  it('has a Board component', () => {
+    expect(cmp.contains(Board)).toBe(true);
   });
 });
